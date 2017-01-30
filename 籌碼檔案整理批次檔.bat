99e992e3968495a86a1996266f51f9e6c7b7e274
Pregaine <wenwei.chung@gmail.com>
2017-01-30 14:08:59 +0800

    1.解決開啟搜集籌碼資料夾太慢問題,改寫載入資料.py,依日期分資料夾儲存 2.加入籌碼檔案整理批次檔

---
+++ 籌碼檔案整理批次檔.bat
@@ -0,0 +1,9 @@
+@echo off
+
+echo 來源路徑 目標路徑
+
+C:\ENV_PY34\Scripts\python.exe D:\03-workspace\02-卷商分點\載入資料.py "D:\\03-workspace\\02-卷商分點\\搜集籌碼資料\\**\\" "D:\\03-workspace\\02-卷商分點\\籌碼資料\\"
+
+echo 整理完成
+
+pause
