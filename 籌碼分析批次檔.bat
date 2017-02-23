57db2aa91e235aca1818291f9905e7bf136f262c
Pregaine <wenwei.chung@gmail.com>
2017-02-23 23:42:32 +0800

    修正換行符號問題在批次檔

--- 籌碼分析批次檔.bat
+++ 籌碼分析批次檔.bat
@@ -1,9 +1,9 @@
 @echo off
-
+echo -------------長期且持續的累積-------------
 echo 來源路徑 查詢股號 目的檔名 結束日期 查詢周期 股本
 
 C:\ENV_PY34\Scripts\python.exe D:\03-workspace\02-卷商分點\籌碼分析.py "D:\\03-workspace\\籌碼資料\\" "1218泰山" "1218泰山籌碼整理" "20170214" 3 3530000000
 
-echo 打完收工
+echo 轉換完成
 
 pause
